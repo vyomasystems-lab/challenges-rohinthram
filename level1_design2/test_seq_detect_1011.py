f93a16a673594e329c37f79f7d7ffd6a970da6f1
rohinthram <rohinthram2014@gmail.com>
2022-07-14 08:10:16 +0000

    level 1 design 2 update

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -23,8 +23,8 @@ async def test_seq_bug1(dut):
     dut.reset.value = 0
     await FallingEdge(dut.clk)
 
-    inp = [1,0,1,1]
-    out = [0,0,0,1]
+    inp = [1,0,1,1,1,0,1,1]
+    out = [0,0,0,1,0,0,0,1]
     for i in range(len(inp)):
         dut.inp_bit.value = inp[i]
         await FallingEdge(dut.clk)
